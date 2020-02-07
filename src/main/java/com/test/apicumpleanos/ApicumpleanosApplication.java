c53f151e06b8766d932b99456903c9996cf7dfc8
Fernando Lobos <fernandolobos@gmail.com>
2020-02-07 15:35:25 -0300

    Initial

---
+++ src/main/java/com/test/apicumpleanos/ApicumpleanosApplication.java
@@ -0,0 +1,13 @@
+package com.test.apicumpleanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicumpleanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicumpleanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/apicumpleanos/ApicumpleanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.apicumpleanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicumpleanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
